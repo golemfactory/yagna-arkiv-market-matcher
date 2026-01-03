60a2dae2e047a5384ee4a579b087745702da9f4a
scx1332 <sieciech@gmail.com>
2026-01-03 23:38:29 +0100

    Working on integration

--- integration/start_and_detach_all.sh
+++ integration/start_and_detach_all.sh
@@ -1,18 +1,19 @@
+#!/bin/bash
 set -x
 
-./start_router.sh &
-./start_matcher.sh &
+/bin/bash start_router.sh &
+/bin/bash start_matcher.sh &
 
 sleep 2
 
 # Start yagna nodes
-./start_provider_node.sh &
-./start_requestor.sh &
+/bin/bash start_provider_node.sh &
+/bin/bash start_requestor.sh &
 
 sleep 10
 
 # Start provider
-./start_provider.sh &
+/bin/bash start_provider.sh &
 
 # Start vanity service
-./start_vanity.sh
+/bin/bash start_vanity.sh
