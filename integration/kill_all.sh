2f5df55c19c74945e5f39ed384361c55f6fd7345
scx1332 <sieciech@gmail.com>
2026-01-05 16:20:50 +0100

    Add kill all script

---
+++ integration/kill_all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "usage: $0 N"
+  exit 1
+fi
+
+end=$(( $1 - 1 ))
+if [ "$end" -lt 0 ]; then
+  echo "no nodes to start"
+  exit 0
+fi
+
+set -x
+
+for i in $(seq 0 "$end"); do
+  pkill ya-provider-upper-"$i"
+  pkill yagna-upper-"$i"
+  pkill vanity-lower-"$i"
+  pkill yagna-lower-"$i"
+done
