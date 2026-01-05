f242a66ec8ac792e7ce2c180625b71619b12db3d
scx1332 <sieciech@gmail.com>
2026-01-05 16:35:01 +0100

    Add log

--- integration/start_and_detach_all.sh
+++ integration/start_and_detach_all.sh
@@ -13,21 +13,21 @@ fi
 set -x
 
 
-/bin/bash start_router.sh &
+./start_router.sh &
 
-# /bin/bash build_matcher.sh
-# /bin/bash start_matcher.sh &
+# ./build_matcher.sh
+# ./start_matcher.sh &
 
 sleep 2
 
 # Start yagna nodes
-/bin/bash start_provider_node.sh "${NUMBER_OF_NODES}" &
-/bin/bash start_requestor.sh &
+./start_provider_node.sh "${NUMBER_OF_NODES}" &
+./start_requestor.sh &
 
 sleep 10
 
 # Start provider
-/bin/bash start_provider.sh "${NUMBER_OF_NODES}" &
+./start_provider.sh "${NUMBER_OF_NODES}" &
 
 # Start vanity service
-/bin/bash start_vanity.sh &
+./start_vanity.sh &
