cfbb18fb0b8dfa54f1ceb86f38cedb4776b503d9
scx1332 <sieciech@gmail.com>
2026-01-06 14:45:21 +0100

    Test central net setup

--- integration/start_router.sh
+++ integration/start_router.sh
@@ -3,4 +3,4 @@ set -x
 
 # Start router
 (cd node-deployer/central-net && cp ya-sb-router router-upper)
-(cd node-deployer/central-net && ./router-upper -l tcp://0.0.0.0:6999)
+(cd node-deployer/central-net && ./router-upper -l tcp://0.0.0.0:6976)
