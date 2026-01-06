ffab387fe4fa71c1d87625616f4734827131c365
scx1332 <sieciech.czajka@golem.network>
2026-01-06 21:22:08 +0100

    scripts

---
+++ integration/start_and_detach_provider.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+  echo "usage: $0 N"
+  exit 1
+fi
+
+NUMBER_OF_NODES=$1
+if [ "$NUMBER_OF_NODES" -lt 0 ]; then
+  echo "no nodes to setup"
+  exit 0
+fi
+
+set -x
+
+
+./start_router.sh &
+
+./build_matcher.sh
+./start_matcher.sh &
+
+sleep 2
+
+# Start yagna nodes
+./start_provider_node.sh "${NUMBER_OF_NODES}" &
+./start_requestor.sh &
+
+sleep 10
+
+# Start provider
+./start_provider.sh "${NUMBER_OF_NODES}" &
+
+# Start vanity service
+./start_vanity.sh &
