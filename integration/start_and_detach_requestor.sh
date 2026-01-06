5dee52f3171643ebcc7fda0185136478224eb1e7
sieci <sieciech@gmail.com>
2026-01-06 21:42:11 +0100

    Split providers and requestors

--- integration/start_and_detach_requestor.sh
+++ integration/start_and_detach_requestor.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
-if [ -z "$1" ]; then
-  echo "usage: $0 N"
-  exit 1
-fi
-
-NUMBER_OF_NODES=$1
-if [ "$NUMBER_OF_NODES" -lt 0 ]; then
-  echo "no nodes to setup"
-  exit 0
-fi
 
 set -x
-
-
-./start_router.sh &
 
 ./build_matcher.sh
 ./start_matcher.sh &
